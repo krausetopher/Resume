c1be8e2bde89978bab847c4fdb75c077eca7ea14
Christopher Krause <79418555+Kraustopher@users.noreply.github.com>
2022-11-15 16:24:13 -0500

    First code commit

---
+++ src/test/java/com/mycompany/myapp/config/AsyncSyncConfiguration.java
@@ -0,0 +1,17 @@
+package com.mycompany.myapp.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.SyncTaskExecutor;
+
+@Configuration
+public class AsyncSyncConfiguration {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        return new SyncTaskExecutor();
+    }
+}
